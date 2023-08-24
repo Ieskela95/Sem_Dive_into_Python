9e5136b1a12bbb812bab46870ae501aa3db11207
Алексей Рулев <108549326+Ieskela95@users.noreply.github.com>
2023-08-24 23:27:26 +0300

    Create task1.py

---
+++ 12seminar/seminar_tasks/task1.py
@@ -0,0 +1,33 @@
+class Factorial:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._arg_archive = []
+            cls._instance._number_archive = []
+            cls._instance._factorial = 1
+        return cls._instance
+
+    def __call__(self, num):
+        self._arg_archive.append(num)
+        self.num = num
+        factorial = 1
+        for i in range(num):
+            factorial *= (i + 1)
+        self._number_archive.append(factorial)
+        return factorial
+
+    def __str__(self):
+        return f'{self._arg_archive}, {self._number_archive}'
+
+    def number_archive(self):
+        return self._arg_archive, self._number_archive
+
+
+fac = Factorial()
+
+print(fac(3))
+print(fac(4))
+print(fac(5))
+print(fac.number_archive())
