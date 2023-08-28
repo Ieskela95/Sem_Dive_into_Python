393781daff3725d0da88a024d95d5d41251924e6
Алексей Рулев <108549326+Ieskela95@users.noreply.github.com>
2023-08-28 17:22:51 +0300

    Create task3.py

---
+++ 13seminar/tasks/task3.py
@@ -0,0 +1,17 @@
+# Создайте класс с базовым исключением и дочерние классы исключения:
+# ○ ошибка уровня,
+# ○ ошибка доступа.
+
+class UserException(Exception):
+    pass
+
+
+class UserLevelError(UserException):
+    pass
+
+
+class UserPermissionError(UserException):
+    pass
+
+
+raise UserLevelError
